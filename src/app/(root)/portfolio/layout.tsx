86b5c9b6af2f4dad56c07d18dcea2eb97fda1c80
Aliu Gloria <glorysamuel75@gmail.com>
2024-05-04 17:27:06 +0100

    updated icon

--- src/app/(root)/portfolio/layout.tsx
+++ src/app/(root)/portfolio/layout.tsx
@@ -3,8 +3,13 @@ import "./style.css";
 
 export const metadata: Metadata = {
   title: "Luxe-Transformation",
-  description: "luxe transformation landing page",
+  description: "Luxe Transformations - Where Comfort Meets Creativity",
+  manifest: "/manifest.json",
+  icons: {
+    apple: "/icon-512x512.png",
+  },
 };
+
 
 export default function RootLayout({
   children,
