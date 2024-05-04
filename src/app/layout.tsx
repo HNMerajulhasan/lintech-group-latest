b2ab5b82a186e4b6d797c986f6d6106b271fcb93
Aliu Gloria <glorysamuel75@gmail.com>
2024-05-04 17:23:50 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,20 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import ReduxProvider from "@/libs/state/reduxProvider";
 import { Archivo } from "next/font/google";
 import NextTopLoader from "nextjs-toploader";
+
 import "./globals.css";
+import type { Metadata } from "next";
+
+
  
-
-
-
 export const metadata: Metadata = {
   title: "Luxe-Transformation",
   description: "Luxe Transformations - Where Comfort Meets Creativity",
   manifest: "/manifest.json",
   icons: {
-    icon: [
-      {
-        url: '/icon-512x512.png',
-        href: '/icon-512x512.png ',
-      },
-    ],
+    apple: "/icon-512x512.png",
   },
-
-  // icons: {
-  //   apple: "/icon-512x512.png",
-  // },
-
-  
 };
 
 
@@ -42,6 +32,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+
+
     <html lang="en" suppressHydrationWarning={true}>
       <body className={archivo.variable}>
         <NextTopLoader color="#c1a457" height={4} />
