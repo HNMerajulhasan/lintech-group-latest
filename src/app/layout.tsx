8b7208c945add58281b469f05047f3f3f536754f
Onyekachi-Chidiebere <onyekachisamuel18@gmai.com>
2024-06-13 07:06:33 +0100

    added google tag  manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,6 +102,17 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16540216891');
+            `,
+          }}
+        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16540216891" />
       </Head>
 
       <body className={archivo.variable}>
